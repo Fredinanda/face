4c3d45812dd96ffba873decb9fc4b3eba8bd2037
fredinanda <megajeka@naver.com>
2015-05-26 15:23:00 +0900

    INDEX MAIN CSS뺌

--- src/com/sds/icto/mysite/repository/MemberDao.java
+++ src/com/sds/icto/mysite/repository/MemberDao.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import com.sds.icto.mysite.domain.*;
 import com.sds.icto.mysite.exception.MemberDaoException;
+
 @Repository
 public class MemberDao {
 

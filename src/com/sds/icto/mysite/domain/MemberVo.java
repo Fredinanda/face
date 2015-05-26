4b9632b988c65df8f4c087846818d76d20fcedbf
assaribari <assariba@gmail.com>
2015-05-26 15:08:51 +0900

    eunho edit

--- src/com/sds/icto/mysite/domain/MemberVo.java
+++ src/com/sds/icto/mysite/domain/MemberVo.java
@@ -3,23 +3,33 @@ package com.sds.icto.mysite.domain;
 public class MemberVo {
 
 	private Long no;
-	private String name;
+	private String firstname;
+	private String lastname;
 	private String email;
 	private String password;
 	private String gender;
-	
-	
+	private String birth;
+	private String workplace;
+	private String address;
+	private String comments;
+	private String sysdate;
 	public Long getNo() {
 		return no;
 	}
 	public void setNo(Long no) {
 		this.no = no;
 	}
-	public String getName() {
-		return name;
+	public String getFirstname() {
+		return firstname;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
 	}
 	public String getEmail() {
 		return email;
@@ -39,6 +49,37 @@ public class MemberVo {
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getWorkplace() {
+		return workplace;
+	}
+	public void setWorkplace(String workplace) {
+		this.workplace = workplace;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getComments() {
+		return comments;
+	}
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	public String getSysdate() {
+		return sysdate;
+	}
+	public void setSysdate(String sysdate) {
+		this.sysdate = sysdate;
+	}
+	
 	
 	
 
